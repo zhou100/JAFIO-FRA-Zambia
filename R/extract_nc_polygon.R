64bb69100fb803cf1bf6b4a433971eb327802066
zhou100 <zhou100@illinois.edu>
2018-10-10 12:05:43 -0500

    updated temperature extraction code and full rainfall

--- R/extract_nc_polygon.R
+++ R/extract_nc_polygon.R
@@ -191,6 +191,9 @@ for(year in start.year:end.year){
    dim(date.3hr)<-c(8,NROW(date.3hr)/8) 
    date.day = date.3hr[1,]
    # format the date into days 
+   # [1] "days since 1900-1-1 00:00:00"
+
+   
    date.day =  as.Date( date.day,origin = "1900-01-01")
    
    # save the date into the 
@@ -220,50 +223,216 @@ extracted.prec = extracted.prec %>% select(date,everything())
 # head(extracted.prec)
 
 # save data and clean the memory 
-save(extracted.prec,file="rain_9016.rda")
+save(extracted.prec,file="data/clean/rain_9016.rda")
 prec_data<-NULL
 
 
-
-## tmax, tmin
+#################################################################################
+### Extract tmax, tmin data 
+#################################################################################
 
 tmax_list = list()
 tmin_list = list()
-for(year in 1979:2016){
+
+# Looping over differnet years. 
+# remember to test for a single year first
+
+start.year = 1990
+end.year = 2016
+
+for(year in start.year:end.year){
+  
+  ##############################################################################
+  # read in the nc weather data for a given year 
+  ##############################################################################
   print(paste0("starting_",year,"_temp"))
-  temp_nc <-nc_open(paste0("/Users/michaelcecil/Documents/mswep/tas_",year ,"_5km_zambia.nc"))
+  start_time <- Sys.time()
+  
+  
+  # Read in the nc data
+  temp_nc <-nc_open(paste0("C://Users//Administrator//Desktop//mswep_temp//tas_",year ,"_5km_zambia.nc"))
+  
+  # save information into separate data frames 
   lat_temp <-ncdf4::ncvar_get(temp_nc, varid="lat")
   lon_temp <- ncdf4::ncvar_get(temp_nc, varid="lon")
   time_temp <-ncdf4::ncvar_get(temp_nc, varid="time")
-  temp_data <- ncvar_get(temp_nc, varid="data")
-  for (i in 1:NROW(zam_ref)){
-    pt_lon <- toString(zam_ref[i]$X_wth)
-    pt_lat <- toString(zam_ref[i]$Y_wth)
-    pt_lat_lon<-paste0(pt_lat,"_",pt_lon)
-
-    windowed_temp <-data.frame(temp_data[lon_temp==(zam_ref[i]$X_wth), lat_temp==(zam_ref[i]$Y_wth) ,])
+  temp_data <- ncdf4::ncvar_get(temp_nc, varid="data")
+  
+  # dim(temp_data)
+  # head(temp_data)
+  
+  ##############################################################################
+  # loop over the points in each district 
+  # for each point in the point data frame, find every day's weather in a given year
+  ##############################################################################
+  for (i in 1:NROW(district.point.df)){
+    #
+    
+    # save the point coordinates temporarily 
+    pt_lon <- district.point.df$lon[i]
+    pt_lat <- district.point.df$lat[i]
+    pt_id = district.point.df$id[i]
+    
+    # find the nearest point location for any given point in your district point data 
+    lon_location = which.min(abs(lon_temp-pt_lon))
+    lat_location = which.min(abs(lat_temp-pt_lat))
+    
+    # save all the weather data in the given point in the dataframe "windowed_temp"
+    windowed_temp <-data.frame(temp_data[lon_location, lat_location,])
     names(windowed_temp) <-c("temp")
-    c<-windowed_temp$temp
-    dim(c)<-c(8,NROW(c)/8) # reshape values
-
-    d_tmax <- data.frame(apply(c, 2, function(x) max(x, na.rm = TRUE))) ## find daily max
-    names(d_tmax) <- c("tmax")
-    e_tmax <- d_tmax - 273.15 # convert to C
-
-    d_tmin <- data.frame(apply(c, 2, function(x) min(x, na.rm = TRUE))) ## find daily min
-    names(d_tmin) <- c("tmin")
-    e_tmin <- d_tmin -273.15 # convert to C
-
-    tmax_list[[pt_lat_lon]] <- rbind(tmax_list[[pt_lat_lon]],e_tmax)
-
-    tmin_list[[pt_lat_lon]]<-  rbind(tmin_list[[pt_lat_lon]],e_tmin)
+    
+    # save the series in a matrix
+    windowed_temp.3hr <-windowed_temp$temp
+    
+    # reshape the long series into a matrix ( 8 columns for 8 reads in a day, and 365 rows for 365 days) 
+    dim(windowed_temp.3hr)<-c(8,NROW(windowed_temp.3hr)/8) 
+    
+    #windowed_temp.daily<- data.frame(colSums(windowed_temp.3hr)) # sum daily precip
+    
+    
+    tmax.daily.K <- data.frame(apply(windowed_temp.3hr, 2, function(x) max(x, na.rm = TRUE))) ## find daily max
+    names(tmax.daily.K) <- c("tmax")
+    tmax.daily.C <- tmax.daily.K - 273.15 # convert to C
+    
+    tmin.daily.K <- data.frame(apply(windowed_temp.3hr, 2, function(x) min(x, na.rm = TRUE))) ## find daily min
+    names(tmin.daily.K) <- c("tmin")
+    tmin.daily.C <- tmin.daily.K -273.15 # convert to C
+    
+    
+    
+    
+    if (year==start.year){
+      tmax_list[[pt_id]] = tmax.daily.C
+      tmin_list[[pt_id]] = tmin.daily.C
+      
+    } else {
+      tmax_list[[pt_id]]<- rbind(tmax_list[[pt_id]],tmax.daily.C)
+      tmin_list[[pt_id]]<- rbind(tmin_list[[pt_id]],tmin.daily.C)
+      
+    }
+    
   } # for point
+  
+  ##############################################################################
+  # Formatting the extracted weather data in a given year 
+  # aggregate the points in each district 
+  ##############################################################################
+  tmax.list.names <- unique(names(tmax_list))
+  tmax.df.point =  data.frame(
+    setNames(
+      lapply(tmax.list.names, function(x) unlist(tmax_list[names(tmax_list) %in% x], 
+                                                 use.names = FALSE)), tmax.list.names))
+  
+  
+  tmax.df.point.transpose= as.data.frame(t(tmax.df.point)) %>%
+    tibble::rownames_to_column(var = "id")
+  
+  # remove the "X" in the data 
+  tmax.df.point.transpose$id = as.character(as.numeric(as.factor(tmax.df.point.transpose$id)))
+  
+  
+  # join the original point data frame to have the district information 
+  tmax.district =  dplyr::inner_join(district.point.df,tmax.df.point.transpose,by="id") %>% 
+    
+    group_by(district) %>%  # aggregate by district
+    
+    select(-id,-lat,-lon) %>% 
+    
+    summarise_all(funs(mean(.))) # average of all the points in the same district
+  
+  # reshape and ready for join 
+  district.names <- as.character(tmax.district$district)
+  tmax.district.date = as.data.frame(t(tmax.district[,-1]))
+  rownames(tmax.district.date) <- NULL
+  colnames(tmax.district.date) <- district.names
+  
+  
+  ################################################################################
+  #### Do the same for tmins 
+  ########################################################################
+  tmin.list.names <- unique(names(tmin_list))
+  tmin.df.point =  data.frame(
+    setNames(
+      lapply(tmin.list.names, function(x) unlist(tmin_list[names(tmin_list) %in% x], 
+                                                 use.names = FALSE)), tmin.list.names))
+  
+  
+  tmin.df.point.transpose= as.data.frame(t(tmin.df.point)) %>%
+    tibble::rownames_to_column(var = "id")
+  
+  # remove the "X" in the data 
+  tmin.df.point.transpose$id = as.character(as.numeric(as.factor(tmin.df.point.transpose$id)))
+  
+  
+  # join the original point data frame to have the district information 
+  tmin.district =  dplyr::inner_join(district.point.df,tmin.df.point.transpose,by="id") %>% 
+    
+    group_by(district) %>%  # aggregate by district
+    
+    select(-id,-lat,-lon) %>% 
+    
+    summarise_all(funs(mean(.))) # average of all the points in the same district
+  
+  # reshape and ready for join 
+  district.names <- as.character(tmin.district$district)
+  tmin.district.date = as.data.frame(t(tmin.district[,-1]))
+  rownames(tmin.district.date) <- NULL
+  colnames(tmin.district.date) <- district.names
+  
+  
+  ##############################################################################
+  # Formatting the date variable and add as a column into the extracted weather data
+  ##############################################################################
+  # reshape the long series into a matrix ( 8 columns for 8 reads in a day, and 365 rows for 365 days) 
+  # divide by 24 to convert date from hours to days 
+  ## Time unit of the temperature nc file: "hours since 2016-1-1 00:00:00"
+  
+  temp.date.3hr = time_temp/24
+  dim(temp.date.3hr)<-c(8,NROW(temp.date.3hr)/8) 
+  temp.date.day = temp.date.3hr[1,]
+  # format the date into days 
+  
+  
+  ## "hours since 2016-1-1 00:00:00"
+  
+  temp.date.day =  as.Date( temp.date.day,origin = paste(as.character(year),"-01-01",sep=""))
+  
+  # save the date into the 
+  tmin.district.date$date = temp.date.day
+  tmax.district.date$date = temp.date.day
+  
+  # clean the temporary list 
+  tmax_list = list()
+  tmin_list = list()
+  
+  
+  # save the data and avoid problems in combing data.
+  if (year==start.year){
+    extracted.tmin = tmin.district.date
+    extracted.tmax = tmax.district.date
+  } else {
+    extracted.tmin = rbind(extracted.tmin,tmin.district.date)
+    extracted.tmax = rbind(extracted.tmax,tmax.district.date)
+    
+  }
+  
+  # counting the processing time 
+  end_time <- Sys.time()
+  print("time for one year is")
+  print(end_time - start_time)
+  
 } # for year
 
-save(tmax_list,file="tmax_list_all_29_redo_test.rda")
-save(tmin_list,file="tmin_list_all_29_redo_test.rda")
 
+# reorder to make date as the first column
+extracted.tmin = extracted.tmin %>% select(date,everything())
+extracted.tmax = extracted.tmax %>% select(date,everything())
+
+# head(extracted.prec)
+
+# save data and clean the memory 
+save(extracted.tmin,file="data/clean/tmin_9016.rda")
+save(extracted.tmax,file="data/clean/tmax_9016.rda")
 temp_data<-NULL
-
 
 
