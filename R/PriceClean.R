dba75c24c99efb4216898af15841ef86828f1cfa
zhou100 <zhou100@illinois.edu>
2019-01-27 23:21:21 -0600

    compile data

--- R/PriceClean.R
+++ R/PriceClean.R
@@ -2,51 +2,102 @@
 ##################################################################
 # packages and functions
 ##################################################################
-
-library(dplyr)
-library(readxl)
-library(ggplot2)
-
+rm(list=ls())
+require(tidyverse)
 source("R/functions/Yearmon.R")
 
 ##################################################################
-# read in the price data 
+# read in the price data (2003-2008)
 ##################################################################
-zam_price <- read_excel("data/maize_prices/zam_price.xlsx")
-#head(zam_price)
+# WFP price from https://data.humdata.org/dataset/wfp-food-prices
+
+# wfpvam_foodprices <- read_csv("C:/Users/Administrator/Downloads/wfpvam_foodprices.csv")
+# zambia_foodprices = wfpvam_foodprices %>% dplyr::filter(adm0_name == "Zambia")
+# save(zambia_foodprices,file="data/maize_prices/zam_price.rda")
+
+load("data/maize_prices/zam_price.rda")
+# select maize price and mealie meals 
+# unique(price.df$cm_name)
+
+# maize meals data only after 2012, choose maize white only 
+maize.white.price = zambia_foodprices %>% 
+   dplyr::select(mkt_name, cm_name,mp_month, mp_price, mp_year) %>% 
+  dplyr::filter(cm_name=="Maize (white) - Retail")  %>%
+  dplyr::select(-cm_name)
+
+
+maize.white.price = yearmon(maize.white.price,year_var = "mp_year",month_var = "mp_month")
+  
+# unique(maize.white.price$mp_year)
+
+# keep the subset of markets that has data in 2003 
+maize2003= maize.white.price %>% dplyr::filter(mp_year == 2003)
+maize2003.mkt = unique(maize2003$mkt_name)
+
+# data on FRA sales available through December 2008 only
+maize.0308= maize.white.price %>% 
+  dplyr::filter(mkt_name %in% maize2003.mkt) %>% 
+  dplyr::filter(mp_year<2009)
+
+
+# check for missings 
+obs.count = maize.0308 %>% group_by(mkt_name) %>% summarise(obs = n())
+unique(obs.count$obs)
+ 
+mkt.complete= obs.count %>% dplyr::filter(obs==72) %>% dplyr::select(mkt_name)
+
+maize.price.complete = maize.0308 %>% 
+  dplyr::filter(mkt_name %in% unique(mkt.complete$mkt_name)) 
+
+# fill in the missing data 
+
+obs.count.missing = maize.0308 %>% group_by(mkt_name) %>% summarise(obs = n()) %>% filter(obs<72 & obs>68)
+obs.count.missing
+ 
+maize.0308.missing = maize.0308 %>% dplyr::filter(mkt_name %in% unique(obs.count.missing$mkt_name))
+
+# output for manual manipulation 
+# write.csv(maize.0308.missing, row.names = FALSE,"data/maize_prices/missing.csv")
+# 
+# maize.0308.lu_mw = read.csv("data/maize_prices/missing_added.csv",stringsAsFactors = FALSE)
+# 
+# maize.0308.lu_mw = yearmon(maize.0308.lu_mw,year_var = "mp_year",month_var = "mp_month")
+
+
+maize.0308.luangwa = maize.0308.missing %>% filter(mkt_name=="Luangwa") %>%  
+   complete(date = seq.Date(min(date), max(date), by="month")) %>%   fill(mkt_name)
+
+
+maize.0308.mwinilunga  = maize.0308.missing %>% filter(mkt_name=="Mwinilunga") %>%
+   complete(date = seq.Date(min(date), max(date), by="month")) %>%  fill(mkt_name)
+
+
+library(imputeTS)
+maize.0308.luangwa$mp_price = na.interpolation(maize.0308.luangwa$mp_price)
+maize.0308.mwinilunga$mp_price = na.interpolation(maize.0308.mwinilunga$mp_price)
+
+# combind and get the master data 
+zambia.maize.master= bind_rows(maize.price.complete,maize.0308.luangwa,maize.0308.mwinilunga) %>% dplyr::select(mkt_name,mp_price,date)
+
+unique(zambia.maize.master$mkt_name)
+
+length(unique(zambia.maize.master$mkt_name))
 
 
 # select the markets where FRA purchases are made 
+fra_purchase <- read_excel("data/data_for_fra_purchases_0203-0910.xls")
+sort(unique(fra_purchase$distname))
+
+condition = unique(zambia.maize.master$mkt_name) %in% unique(fra_purchase$distname)
+
+unique(zambia.maize.master$mkt_name)[!condition]
+
+# "Kabwe Rural"  "Kabwe Urban"  "Ndola Rural"  "Lusaka Rural" "Lusaka Urban"
+
+zambia.maize.master$mkt_name[zambia.maize.master$mkt_name =="Kabwe Urban"] = "Kabwe_urban"
+zambia.maize.master$mkt_name[zambia.maize.master$mkt_name =="Lusaka Urban"] = "Lusaka_urban"
 
 
-
-
-
-
-
-# select Lusaka price 
-zam_price_clean1217 = zam_price %>% filter(mkt_name=="Lusaka") %>% select(mp_month,mp_year,mp_price)
-zam_price_clean0312 = zam_price %>% filter(mkt_name=="Lusaka Urban") %>% select(mp_month,mp_year,mp_price)
-zam_price_clean = bind_rows(zam_price_clean0312,zam_price_clean1217)
-
-# transform year and month to date
-zam_price_yearmon = yearmon(zam_price_clean,year_var = "mp_year",month_var = "mp_month") %>% select(-mp_month,-mp_year,-yearmon) %>% distinct()  
-# reorder the columns
-zam_price_yearmon = zam_price_yearmon[,c("date","mp_price")]
-
-# check for missings 
-date =  seq(zam_price_yearmon$date[1],length=185,by="+1 month")
-dplyr::left_join(as.data.frame(date),zam_price_yearmon,by ="date")
-
-# read in the missing data covered by fews net price bullentin
-zam_price18 <- read_excel("data/maize_prices/zam_price18.xlsx")
-zam_price18$date = as.Date(zam_price18$date)
-zam_price_filled  = dplyr::bind_rows(zam_price18,zam_price_yearmon) %>% arrange(date)
-
-
-# read in rsa price 
-rsa_price <- read_excel("data/maize_prices/rsa_price_clean.xlsx")
-rsa_price$date = as.Date(rsa_price$date )
 
 ##################################################################
 # adjust by cpi 
@@ -129,8 +180,23 @@ Price = left_join(zam_price_final,rsa_price_kwacha,by = "date")
 rsa_to_zam <- read_excel("data/trade_flow/rsa_to_zam.xlsx")
 zam_to_rsa <- read_excel("data/trade_flow/zam_to_rsa.xlsx")
 
-rsa_to_zam = rsa_to_zam %>% select(YEAR,MONTH,VALUE,QUANTITY)
-zam_to_rsa = zam_to_rsa %>% select(YEAR,MONTH,VALUE,QUANTITY)
+rsa_to_zam = yearmon(rsa_to_zam,year_var = "YEAR",month_var = "MONTH")
+zam_to_rsa = yearmon(zam_to_rsa,year_var = "YEAR",month_var = "MONTH")
+ 
+rsa_to_zam.fill = rsa_to_zam %>% select(YEAR,MONTH,VALUE,QUANTITY,date) %>%  
+  complete(date = seq.Date(min(date), max(date), by="month")) %>% 
+  mutate(YEAR = format(date,"%Y"),MONTH= as.numeric(format(date,"%m")))
+  
+
+zam_to_rsa.fill = zam_to_rsa %>% select(YEAR,MONTH,VALUE,QUANTITY,date) %>% 
+  complete(date = seq.Date(as.Date("1996-10-01"), as.Date("2017-02-01"), by="month")) %>% 
+  mutate(YEAR = format(date,"%Y"),MONTH= as.numeric(format(date,"%m")))
+
+
+
+
+complete(Date = seq.Date("1996-10-01", "2008-12-01", by="month"))
+
 
 
 yearmon(rsa_to_zam,year_var = "YEAR",month_var = "MONTH")
@@ -146,4 +212,3 @@ require("vars")
 
 # weather data 
 
-# 
